81466a7629bd0be384f91fca4687ceb568d8c4f5
Bartol <freskura.bartol@gmail.com>
2015-09-15 23:29:32 +0200

    added new comments

--- R/weights_calculation.R
+++ R/weights_calculation.R
@@ -2,17 +2,24 @@
 #' @name Calculate weights for portfolio
 #' @author Bartol Freškura
 #' @description Returns data frame containing all combinations of weights where
-#' sum of the weigths in each row equals one.
+#' sum of the weigths in each row equals between 0.97 and 1.
+#' WARNING: This function has a high RAM memory usage so use with caution
+#' because it can easily take all your CPU and RAM resources and therfore
+#' freeze your computer.
 #' @param stockNumber number of stock in portfolio
 #' @param increment Increment when determining weights
-#' @return 
+#' @return data frame containing all possible combinations of weights which
+#' when summed equal between 0.97 and 1.
 getWeights <- function(increment=0.1, stockNumber=3) {
-      
-      
-      #Precaution
-      if(increment<0.05 || stockNumber>7){
+
+
+      #Precaution because of the high complexity
+      if(increment<0.02 | stockNumber>6){
             break;
-      }     
+      }
+
+      ##Tested with inc=0.02 and s#= 4.
+      ##Tested with inc=0.03 and s#= 5.
       else {
             increments <- seq(from = 0, to= 1, by=increment)
             frame <- expand.grid(rep(list(increments),each=stockNumber))
@@ -21,4 +28,4 @@ getWeights <- function(increment=0.1, stockNumber=3) {
       }
 }
 
- 
+
